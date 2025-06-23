322a3819974287fbed76c9ad20257b768f786108
wxbujiming <wxbujiming@tutamail.com>
2025-06-23 08:07:37 +0000

    20250623第二次提交(main): 更新 demo.py 文件注释
    - 将"测试打包镜像"注释更改为"测试打包镜像第二次"
    - 该更改无实际功能影响，仅用于演示提交过程

--- main/demo.py
+++ main/demo.py
@@ -5,7 +5,7 @@ import time
 if __name__ == '__main__':
     # 主程序入口
     # 持续输出当前时间的无限循环
-    # 测试打包镜像
+    # 测试打包镜像第二次
     while True:
         # 获取当前东八区时间并格式化为字符串(精确到毫秒)
         time_str = datetime.now(timezone(timedelta(hours=8))).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3] 
